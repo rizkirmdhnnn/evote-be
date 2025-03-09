20dc2032615f243ed80a7649e2b2b545e35e5fd4
Achmad Rizki Ramadhan <achmadrizkiramadhan0101@gmail.com>
2025-03-10 01:16:18 +0700

    refactor: group routes for easier reading

--- routes/api.go
+++ routes/api.go
@@ -9,17 +9,21 @@ import (
 
 func Api() {
 	authController := controllers.NewAuthController()
+	pollsController := controllers.NewPollsController()
+	optionController := controllers.NewOptionController()
+
+	// @Group Auth
 	facades.Route().Post("/auth/register", authController.Register)
 	facades.Route().Post("/auth/login", authController.Login)
 
-	pollsController := controllers.NewPollsController()
+	// @Group Polls
 	facades.Route().Middleware(middleware.Auth()).Get("/polls", pollsController.Index)
 	facades.Route().Middleware(middleware.Auth()).Post("/polls/create", pollsController.Store)
 	facades.Route().Middleware(middleware.Auth()).Get("/polls/{id}", pollsController.Show)
 	facades.Route().Middleware(middleware.Auth()).Put("/polls/{id}/update", pollsController.Update)
 	facades.Route().Middleware(middleware.Auth()).Delete("/polls/{id}/delete", pollsController.Delete)
 
-	optionController := controllers.NewOptionController()
+	// @Group Options
 	facades.Route().Middleware(middleware.Auth()).Post("/options/create", optionController.Store)
 	facades.Route().Middleware(middleware.Auth()).Delete("/options/{id}/delete", optionController.Delete)
 }
